9ad098c777650f8d382f38da89689f4aca900d94
Ivan Dubrov <dubrov.ivan@gmail.com>
2017-09-14 00:11:54 -0700

    Fixing documentation

--- src/lib.rs
+++ src/lib.rs
@@ -17,9 +17,9 @@
 //!
 //! let mut stepper = Stepgen::new(1_000_000);
 //!
-//! stepper.set_acceleration(1000 << 8).unwrap(); // 1200 steps per second per second
+//! stepper.set_acceleration(1000 << 8).unwrap(); // 1000 steps per second per second
 //! stepper.set_target_step(1000); // stop at step 1000
-//! stepper.set_target_speed(800 << 8).unwrap(); // 240RPM (4 turns per second)
+//! stepper.set_target_speed(800 << 8).unwrap(); // 800 steps per second (4 turns per second)
 //!
 //! // Take 99 steps
 //! for _ in 0..99 {
@@ -28,7 +28,7 @@
 //!
 //! assert_eq!(99, stepper.current_step());
 //! assert_eq!(113621, stepper.current_speed());
-//! assert_eq!(2242, (stepper.next().unwrap() + 128) >> 8); // delay for 100th step, rounded to nearest integer
+//! assert_eq!(2242, (stepper.next().unwrap() + 128) >> 8); // delay for 100th step, rounded to the nearest integer
 //! ```
 //! ## Note on numbers
 //!
